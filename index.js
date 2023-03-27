7ddff73111dfc0e6317b5f4a38cbf5adfc453fb6
aml <amadeu@dfsi.pt>
2023-03-27 19:07:15 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http")
+
+const PORT = process.env.PORT || 8080
+
+const server = http.createServer()
+
+server.on("request", (request, response) => {
+    // handle request based on method then URL
+    response.statusCode = 200
+    response.write("Hello World")
+    response.end()
+})
+
+server.listen(PORT, err => {
+    // error checking
+    err ? console.error(err) : console.log(`listening on port ${PORT}`)
+})
